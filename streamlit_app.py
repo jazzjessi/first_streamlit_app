368d110ec4a84e0b5fac6a689e5acef217fae02f
Jessica Balzer <148968302+jazzjessi@users.noreply.github.com>
2023-12-01 13:31:56 +0100

    Update streamlit_app.py
    more text

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,12 @@ streamlit.title('My Parents new healthy Diner')
 
 streamlit.header('Breakfast Menu')
 
-streamlit.text('Omega 3 & Blurberry Oatmeal')
+streamlit.text('🥣 Omega 3 & Blurberry Oatmeal')
 
-streamlit.text('Kale, Spinach& Rockete Smoothie')
+streamlit.text('🥗 Kale, Spinach& Rockete Smoothie')
 
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
