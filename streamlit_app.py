5c232552d168bbf313768d85ef4fcf5ee1af018a
Jessica Balzer <148968302+jazzjessi@users.noreply.github.com>
2023-12-04 13:16:07 +0100

    Update streamlit_app.py
    try except mit if else

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,15 +5,10 @@ import snowflake.connector
 from urllib.error import URLError
 
 streamlit.title('My Parents new healthy Diner')
-
 streamlit.header('Breakfast Menu')
-
 streamlit.text('🥣 Omega 3 & Blurberry Oatmeal')
-
 streamlit.text('🥗 Kale, Spinach& Rockete Smoothie')
-
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
@@ -31,16 +26,21 @@ streamlit.dataframe(fruits_to_show)
 
 # new section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
 
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+except URLError as e:
+  streamlit.error()
 
-# normalize json format? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# show in table?
-streamlit.dataframe(fruityvice_normalized)
+
+
+
 
 # don't run anything past here while we troubleshoot
 streamlit.stop()
